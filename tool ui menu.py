759973b3406c05c4c800a714d3f7826842efec51
FrankieHobbins <acepainkiller@gmail.com>
2017-12-19 11:42:21 +0000

    Add files via upload
    automerge from groups should cope with objects having modifiers now

--- tool ui menu.py
+++ tool ui menu.py
@@ -369,6 +369,24 @@ def export_fbx_anim_old():
     currentscene.render.fps = 30
     bpy.context.object.animation_data.action = currentaction
     return()
+
+def copyModifier(source, target):
+    active_object = source
+    obj = target
+
+    for mSrc in active_object.modifiers:
+        mDst = obj.modifiers.get(mSrc.name, None)
+        if not mDst:
+            mDst = obj.modifiers.new(mSrc.name, mSrc.type)
+
+        # collect names of writable properties
+        properties = [p.identifier for p in mSrc.bl_rna.properties
+                      if not p.is_readonly]
+
+        # copy those properties
+        for prop in properties:
+            setattr(mDst, prop, getattr(mSrc, prop))
+
     
 #######################
 ###### OPERATORS ######
@@ -378,7 +396,7 @@ class AutoMergeGroups(bpy.types.Operator):
     """Replaces mesh of object name X with all objects in group name X"""
     bl_idname = "bone.automergegroups"
     bl_label = "Unused"
-    
+
     def execute(self, context):
         print ("-----------------------------")
         scn = bpy.context.scene
@@ -387,16 +405,16 @@ class AutoMergeGroups(bpy.types.Operator):
         groups = []
 
         #check seleted objects have groups
-        for group in bpy.data.groups:            
+        for group in bpy.data.groups:
             for o in selobj:
                 if group.name == o.name:
                     #make a list of all objects in group
-                    print ("starting " + group.name)        
+                    print ("starting " + group.name)
                     objInGroup = []
                     for obj in group.objects:
                         objInGroup.append(obj)
-                    
-                    #find object corresponding to group (object to replace has to be named the same as group)    
+
+                    #find object corresponding to group (object to replace has to be named the same as group)
                     for obj in bpy.data.objects:
                         if obj.name == group.name:
                             print("found object for "+ obj.name)
@@ -408,14 +426,16 @@ class AutoMergeGroups(bpy.types.Operator):
                     objInGroupNew = []
                     for obj in objInGroup:
                         print (obj.name) 
-                        if obj.name != objName:                
+                        if obj.name != objName:
                             objData = obj.data.copy()         #get data from object
 
                             ob = bpy.data.objects.new("MergeMe", objData)   #add that data to a new object
-                            ob.matrix_world = obj.matrix_world                      #in the same place as the old one                                    
-
+                            ob.matrix_world = obj.matrix_world              #in the same place as the old one
+                                                 
                             scn.objects.link(ob)        #add to scene
                             objInGroupNew.append(ob)    #add to list of NEW objects
+                            copyModifier(obj,ob)
+                            
                             scn.update()        
 
                     #rename old object ready for delete
@@ -426,11 +446,15 @@ class AutoMergeGroups(bpy.types.Operator):
                         for objG in objInGroupNew:
                             if obj.name in objG.name:
                                 obj.select = True 
+                                bpy.context.scene.objects.active = obj
                                 break
                             obj.select = False
 
                     #apply scale and transform info
                     bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
+                    
+                    #apply modifiers(have been copied)
+                    bpy.ops.object.convert(target='MESH')
                                 
                     #make a new object to merge all new objects into, add to scene and set transform based on old one
                     me = bpy.data.meshes.new(group.name)
@@ -441,16 +465,16 @@ class AutoMergeGroups(bpy.types.Operator):
                     #select this new object
                     ob.select = True
                     bpy.context.scene.objects.active = ob  #has to be active so join works
-                    
+
                     #merge all new objects into one
                     bpy.ops.object.join()
-                    
+
                     #copy layer info and modifiers from old models to new one
                     ob.layers = oldObj.layers
                     oldObj.select = True
                     bpy.context.scene.objects.active = oldObj
                     bpy.ops.object.make_links_data(type="MODIFIERS")        
-                    
+
                     #remove old object
                     scn.objects.unlink(oldObj)
                     oldObj.user_clear()        
